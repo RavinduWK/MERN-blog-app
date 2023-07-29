32e3a156b55b6554e2b3d5ed8ea2d9deb16bebfd
leadpro456 <ravinduyasiska@gmail.com>
2023-07-29 12:53:53 +0530

    Ready to deploy 4

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,7 +6,7 @@ export default function Post({_id, title, summary, cover, content, createdAt, au
         <div className="post">
             <div className="image">
                 <Link to={`/post/${_id}`}>
-                    <img src={"https://wickramblogs-api.onrender.com//"+cover} />
+                    <img src={"https://wickramblogs-api.onrender.com/"+cover} />
                 </Link>
             </div>
             <div className="texts">
